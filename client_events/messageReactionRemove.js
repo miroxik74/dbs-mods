eb62759d335b09b9aafe0f6bd4b9ca52d92d2742
miroxik74 <mirek11115@gmail.com>
2024-12-09 18:56:22 +0100

    brrr

---
+++ client_events/messageReactionRemove.js
@@ -0,0 +1,112 @@
+/* To make it work:
+1. Open AAA_BetterMods.js
+2. Find:
+3. Make new line and paste:
+4. Results:
+5. Create a file with any name, this will be your loader to fetch desired channel, message and reactions from it
+6. Create a file with any name, this will be the actual code that is executed when someone removed reaction
+> for unicode emoji you can use https://getemoji.com/ or https://pl.piliapp.com/emoji/list/
+
+That's all you need to know, thanks for reading and good luck :)
+*/
+
+// 2.
+DBS.Bot = new Client({
+
+// 3.
+partials: [
+    "CHANNEL",
+    "MESSAGE",
+    "REACTION"
+],
+            
+// 4.
+        DBS.Bot = new Client({
+            partials: [
+                "CHANNEL",
+                "MESSAGE",
+                "REACTION"
+            ],
+            intents: [
+                Intents.FLAGS.GUILDS,
+                Intents.FLAGS.GUILD_MEMBERS,
+                Intents.FLAGS.GUILD_BANS,
+                Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+                Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+                Intents.FLAGS.GUILD_VOICE_STATES,
+                Intents.FLAGS.GUILD_MESSAGES,
+                Intents.FLAGS.GUILD_MESSAGE_TYPING,
+                Intents.FLAGS.DIRECT_MESSAGES,
+                Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+                Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+            ]
+        });
+
+// 5.
+module.exports = {
+    name: "ready",
+    author: ["334671104704774154"],
+    version: "1.0.0",
+    changelog: "for 556130039876419604",
+    isEvent: true,
+    isResponse: false,
+    isMod: true,
+    isAddon: false,
+    section: "",
+    html: function (data) {
+        return `
+        `;
+    },
+    init: async function (DBS) {
+    },
+    mod: async function (DBS) {
+        const channel = await DBS.channels.fetch('SET_CHANNEL_ID');
+        const message = await channel.messages.fetch('SET_MESSAGE_ID');
+        const reactions = await message.reactions.cache.get();
+    }
+};
+
+// 6.
+module.exports = {
+    name: "messageReactionRemove",
+    author: ["334671104704774154"],
+    version: "1.0.0",
+    changelog: "for 556130039876419604",
+    isEvent: true,
+    isResponse: false,
+    isMod: true,
+    isAddon: false,
+    section: "",
+    html: function (data) {
+        return `
+        `;
+    },
+    init: async function (DBS) {
+    },
+    mod: async function (DBS, reaction, user) {
+        const channelId1 = 'SET_CHANNEL_ID';
+        const messageId1 = 'SET_MESSAGE_ID';
+        const message = reaction.message;
+        const emoji = reaction.emoji.name;
+        const guild = await message.guild.fetch();
+        const channel = await guild.channels.fetch(channelId1);
+        await channel.messages.fetch(messageId1);
+
+        try {
+            if (message.id === messageId1 && channel.id === channelId1) {
+                const userHasReacted = await message.guild.members.fetch(user.id);
+
+                // for unicode you can use https://getemoji.com/ or https://pl.piliapp.com/emoji/list/
+                if (emoji === 'SET_EMOJI_NAME_OR_PASTE_IF_UNICODE') {
+                    const role = userHasReacted.guild.roles.cache.get('SET_ROLE_ID');
+                    if (role && userHasReacted) {
+                        await userHasReacted.roles.remove(role);
+                    }
+                }
+
+            }
+        } catch (error) {
+            return console.log(error);
+        }
+    }
+};
