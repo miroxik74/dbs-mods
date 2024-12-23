799d3e1d2ba8f81921870bac8aae78788f336a2e
miroxik74 <mirek11115@gmail.com>
2024-12-23 11:59:40 +0100

    brrr

--- mods/shard.js
+++
@@ -1,8 +0,0 @@
-// sharding.js
-const Discord = require('discord.js');
-const Manager = new Discord.ShardingManager('./bot.js');
-
-Manager.spawn(4);
-Manager.on("shardCreate", async (shard) => {
-    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${shard.id}`);
-});
