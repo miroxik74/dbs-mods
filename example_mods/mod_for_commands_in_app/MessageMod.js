20dcd0584eab3998edb1456065dd3ba99f354544
miroxik74 <mirek11115@gmail.com>
2024-12-26 21:33:36 +0100

    brrr

--- example_mods/mod_for_commands_in_app/MessageMod.js
+++ example_mods/mod_for_commands_in_app/MessageMod.js
@@ -1,33 +1,15 @@
 module.exports = {
-    // Set this to the name of the mod. This is what will be shown inside of Discord Bot Studio.
-    // THIS FILE NAME MUST BE THIS VALUE WITH SPACES REMOVED
     name: "Message Mod",
-
-    // Place the author of the mod here. This is an array so you can add other authors by writing ["Great Plains Modding", "New User"]
     author: ["Discord Bot Studio"],
-
-    // Place the version of the mod here.
     version: "1.0.0",
-
-    // Whenever you make a change, please place the changelog here with your name. Created Send Message ~ Great Plains Modding\n
     changelog: "Created Send Message ~ Great Plains Modding",
-
-    // Set this to true if this will be an event. Note events wont show up in DBS.
     isEvent: false,
-    
     isResponse: true,
-
-    // Set this to true if this will be a response mod.
     isMod: true,
-
-    // If you want to modify a core feature, set this to true.
     isAddon: false,
-
-    // Here you can define where you want your mod to show up inside of Discord Bot Studio
     section: "Message",
 
-    // Place your html to show inside of Discord Bot Studio when they select your mod.
-    html: function(data) {
+    html: function (data) {
         return `
             <div class="form-group">
                 <label>Send To *</label>
@@ -44,13 +26,11 @@ module.exports = {
         `;
     },
 
-    // When the bot is first started, this code will be ran.
-    init: function() {
-        console.log("Loaded send message");
+    init: function () {
+        console.log("Loaded Message Mod");
     },
 
-    // Place your mod here.
-    mod: function(DBS, message, action, args, command, index) {
+    mod: function (DBS, message, action, args, command, index) {
 
         // Note DBS stores all data from the HTML field into lowercase. messageText = messagetext
         switch (action.channelname) {
