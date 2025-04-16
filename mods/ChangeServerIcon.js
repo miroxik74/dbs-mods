0291d7ee679613a5bafb9a8332b3f7a32a24c56f
miroxik74 <mirek11115@gmail.com>
2025-04-17 00:33:21 +0200

    brrr

--- mods/ChangeServerIcon.js
+++ mods/ChangeServerIcon.js
@@ -1,27 +1,44 @@
 module.exports = {
     name: "ChangeServerIcon",
-    author: ["aoe#9022"],
-    version: "1.0.0",
-    changelog: "Added ChangeServerIcon Mod",
+    author: ["aoe#9022", "@miroxik74"],
+    version: "1.0.1",
+    changelog: "Added variables and fixed getting icon url",
     isEvent: false,
     isResponse: true,
     isMod: true,
     isAddon: false,
     section: "Server Action",
-    html: function(data) {
+    html: function (data) {
         return `
-        <div class="form-group">
-            <label>Link of the new Server Profile(works with gifs too) *</label>
-            <textarea class="form-control needed-field" name="pfp" rows="1" ></textarea>
+        </div>
+            </div>
+                <div class="col">
+                <label>Link of the new Server Profile(works with gifs too)</label>
+                    <div class="input-group mb-3">
+                        <input class="form-control needed-field" name="icon"></input><br>
+                        <div class="input-group-append">
+                        <a class="btn btn-outline-primary" role="button" id="variables" forinput="icon">Insert Variable</a>
+                </div>
+            </div>
+            <p class="text-muted">Note:<br>Some images just won't work due to size or format so just use another url</p>
         </div>
         `;
     },
-    init: function() {
-        console.log("Loaded ChangeServerIcon Mod");
+    init: async function (DBS) {
+        if (!DBS.BetterMods) return console.log(`\x1b[36m [${this.name}.JS] \x1b[0m\x1b[31mBetterMods.js is not loaded. BetterMods.js is required to use this mod. \x1b[0m`);
+
+        console.log("Loaded ChangeServerIcon");
     },
-    mod: function(DBS, message, action, args, command, index) {
-        message.guild.setIcon(action.pfp) // changes server pfp
-        // This script is one of the Longest Scripts ive ever made lmao
+    mod: async function (DBS, message, action, args, command, index) {
+
+        try {
+            const icon = DBS.BetterMods.parseAction(action.icon, message);
+            await message.guild.setIcon(icon);
+        } catch (error) {
+            await message.reply(`${error}`);
+            console.log(error);
+        }
+
         DBS.callNextAction(command, message, args, index + 1);
     }
 };
