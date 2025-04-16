de95a573477e4f2937542f700b930bf814fcc1b1
miroxik74 <mirek11115@gmail.com>
2025-04-17 01:56:36 +0200

    brrr

--- mods/ChangeServerIcon.js
+++ mods/ChangeServerIcon.js
@@ -35,7 +35,7 @@ module.exports = {
     },
     init: async function (DBS) {
         if (!DBS.BetterMods) return console.log(`\x1b[36m [${this.name}.JS] \x1b[0m\x1b[31mBetterMods.js is not loaded. BetterMods.js is required to use this mod. \x1b[0m`);
-        console.log("Loaded ChangeServerIcon");
+        console.log("Loaded Change Server Icon");
     },
     mod: async function (DBS, message, action, args, command, index) {
 
